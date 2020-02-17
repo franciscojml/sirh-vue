4a8b2a4b9c968b8f7228bef9af9e755e626f0390
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2020-02-17 14:08:28 -0300

    método save

--- backend/api/dadoscadastrais.js
+++ backend/api/dadoscadastrais.js
@@ -10,18 +10,6 @@ module.exports = app => {
         const count = parseInt(result[0].total)
 
         app.db('SIRH.SRHTB002')
-            .select({matricula: 'MATRICA'}, {tipoContrato: 'TIPOCONT'}, {nome: 'NOMEFUNC'}, {dataNascimento: 'DATANASC'},
-            {cidadeNascimento: 'NATURCID'}, {naturalidade: 'NATURUF'}, {nascionalidade: 'NACIONA'}, {estadoCivil: 'ESTACIVI'},
-            {sexo: 'SEXOFUN'}, {rg: 'CIDNUME'}, {orgaoExpedidorRg: 'CIDORGAO'}, {ufRg: 'CIDUNFED'},
-            {dataExpedicaoRg: 'CIDDATA'}, {cpf: 'CPFFUNC'}, {tituloEleitor: 'NUMTITEL'}, {zona: 'ZONTITEL'},
-            {secao: 'SECTITEL'}, {votou1Turno: 'VOTITEL1'}, {votou2Turno: 'VOTITEL2'}, {nomePai: 'NOMEPAI'},
-            {nomeMae: 'NOMEMAE'}, {rua: 'ENDRUAF'}, {bairro: 'ENDBAIF'}, {cidade: 'ENDCIDF'},
-            {estado: 'ENDESTF'}, {cep: 'ENDCEPF'}, {ddd: 'DDDFUNC'}, {telefone: 'TELEFUNC'},
-            {nivelGrauInstrucao: 'INSTNIVE'}, {situacaoGrauInstrucao: 'INSTSITU'}, {dataAdmissao: 'DATAADIM'}, {lotacaoAtual: 'LOTATUAL'},
-            {sublotacaoAtual: 'SUBLOTATUAL'}, {jornada: 'JORNADA'}, {carteiraTrabalho: 'CARTNUME'}, {ufCarteiraTrabalho: 'CARTESTA'},
-            {serieCarteiraTrabalho: 'CARTSERI'}, {banco: 'BANCOFUN'}, {agencia: 'AGENCFUN'}, {contaBancaria: 'CONTAFUN'},
-            {codigoAfastamento: 'AFASCODI'}, {dataInicioAfastamento: 'AFASINIC'}, {dataTerminoAfastamento: 'AFASTERM'}, {percentagemReciboSeguro: 'SEGPGTO'},
-            {matriculaSiapi: 'SP_MATRICA'}, {dataCarteiraTrabalho: 'CARTDATA'})
             .orderBy('NOMEFUNC', 'asc')
             .limit(limit).offset(page * limit - limit)
             .then(empregado => res.json({ data: empregado, count, limit }))
@@ -29,33 +17,76 @@ module.exports = app => {
     }
 
     
-   const save = (req, res) => {
+   const save = async (req, res) => {
         const dadoscadastrais = { ...req.body }
-        if(req.params.id) dadoscadastrais.matrica = req.params.id
 
         try {
-            existsOrError(dadoscadastrais.name, 'Nome não informado')
-            existsOrError(article.description, 'Descrição não informada')
-            existsOrError(article.categoryId, 'Categoria não informada')
-            existsOrError(article.userId, 'Autor não informado')
-            existsOrError(article.content, 'Conteúdo não informado')
+            validationCamposObrigatorios(dadoscadastrais)
         } catch(msg) {
             res.status(400).send(msg)
         }
 
-        if(dadoscadastrais.matrica) {
-            app.db('articles')
-                .update(article)
-                .where({ id: article.id })
+        const dc = await app.db('SIRH.SRHTB002')
+                            .where({ MATRICA: dadoscadastrais.matrica }).first()
+                            
+        if(dc) {
+            app.db('SIRH.SRHTB002')
+                .update(dadoscadastrais)
+                .where({ MATRICA: dadoscadastrais.matrica })
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
-            app.db('articles')
-                .insert(article)
+            app.db('SIRH.SRHTB002')
+                .insert(dadoscadastrais)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         }
     }
+
+    function validationCamposObrigatorios(obj){
+        existsOrError(obj.MATRICA, 'Matrícula não informado')
+        existsOrError(obj.TIPOCONT, 'Tipo de Contrato não informado')
+        existsOrError(obj.NOMEFUNC, 'Nome não informado')
+        existsOrError(obj.NATURCID, 'Cidade de Nascimento não informado')
+        existsOrError(obj.NATURUF, 'Naturalidade não informado')
+        existsOrError(obj.NACIONA, 'Nascionalidade não informado')
+        existsOrError(obj.ESTACIVI, 'Estado Civil não informado')
+        existsOrError(obj.SEXOFUN, 'Sexo não informado')
+        existsOrError(obj.CIDNUME, 'RG não informado')
+        existsOrError(obj.CIDORGAO, 'Órgão Expedidor do Rg não informado')
+        existsOrError(obj.CIDUNFED, 'UF do Rg não informado')
+        existsOrError(obj.CPFFUNC, 'UF do Rg não informado')
+        existsOrError(obj.NUMTITEL, 'Título de Eleitor não informado')
+        existsOrError(obj.ZONTITEL, 'Zona do título de Eleitor não informado')
+        existsOrError(obj.SECTITEL, 'Seção do título de Eleitor não informado')
+        existsOrError(obj.VOTITEL1, 'Indicação de votação no 1º Turno não informado')
+        existsOrError(obj.VOTITEL2, 'Indicação de votação no 2º Turno não informado')
+        existsOrError(obj.NOMEPAI, 'Nome do Pai não informado')
+        existsOrError(obj.NOMEMAE, 'Nome da Mãe não informado')
+        existsOrError(obj.ENDRUAF, 'Rua do endereço não informado')
+        existsOrError(obj.ENDBAIF, 'Bairro do endereço não informado')
+        existsOrError(obj.ENDCIDF, 'Cidade do endereço não informado')
+        existsOrError(obj.ENDESTF, 'Estado do endereço não informado')
+        existsOrError(obj.ENDCEPF, 'CEP do endereço não informado')
+        existsOrError(obj.DDDFUNC, 'DDD do Telefone não informado')
+        existsOrError(obj.TELEFUNC, 'Telefone não informado')
+        existsOrError(obj.INSTNIVE, 'Nível de Grau de Instrução não informado')
+        existsOrError(obj.INSTSITU, 'Situação de Grau de Instrução não informado')
+        existsOrError(obj.DATAADIM, 'Data de Admissão não informado')
+        existsOrError(obj.LOTATUAL, 'Unidade de lotação atual não informado')
+        existsOrError(obj.SUBLOTATUAL, 'Unidade de sub-lotação atual não informado')
+        existsOrError(obj.JORNADA, 'Jornade de trabalho (h) não informado')
+        existsOrError(obj.CARTNUME, 'Número da Carteira de Trabalho não informado')
+        existsOrError(obj.CARTESTA, 'UF da Carteira de Trabalho não informado')
+        existsOrError(obj.CARTSERI, 'Série da Carteira de Trabalho não informado')
+        existsOrError(obj.BANCOFUN, 'Banco não informado')
+        existsOrError(obj.AGENCFUN, 'Agência não informado')
+        existsOrError(obj.CONTAFUN, 'Conta Corrente não informado')
+        existsOrError(obj.AFASCODI, 'Código de Afastamento não informado')
+        existsOrError(obj.SEGPGTO, 'Percentagem de Recibo do Seguro não informado')
+        existsOrError(obj.SP_MATRICA, 'Matrícula do SIAPI não informado')
+    }
+
 
     return { get, save }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,4 +4,5 @@ module.exports = app => {
 
     app.route('/api/dadoscadastrais')
         .get(app.api.dadoscadastrais.get)
+        .put(app.api.dadoscadastrais.save)
 }
