dc8a3a1416a23c29385274819791bb58f5a82fb7
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2020-03-26 11:36:10 -0300

    ajuestes

--- backend/index.js
+++ backend/index.js
@@ -17,35 +17,6 @@ consign()
     .then('./config/routes.js')
     .into(app)
 
-// swagger definition
-/*var swaggerDefinition = {
-    info: {
-      title: 'Documentação da API do SIRH',
-      version: '1.0.0',
-      description: 'API referente aos resources disponíveis para manipulação das informações do SIRH',
-    },    
-    basePath: '/'
-  };
-  
-  // options for the swagger docs
-  var options = {
-    // import swaggerDefinitions
-    swaggerDefinition: swaggerDefinition,
-    // path to the API docs
-    apis: ['./config/routes.js'],
-    securityDefinitions: { },
-  };
-  
-  // initialize swagger-jsdoc
-  var swaggerSpec = swaggerJSDoc(options);
-
-  app.get('/swagger.json', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-  });
-
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));*/
-
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   var getLDAPConfiguration = function (req, callback) {
