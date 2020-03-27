1d7be55d73897ca5db2bfa35c87f2d3dab607b22
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2020-03-27 11:51:11 -0300

    Ajuste Ldap 3

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,13 +2,7 @@ const admin = require('./admin')
 
 module.exports = app => {
 
-    
-
-    app.post('/signin', app.api.auth.signin)
-
     app.route('/api/dadoscadastrais')
         .get(app.api.empregado.get)
-        //.put(app.api.dadoscadastrais.save)
-
-    
+    //.put(app.api.dadoscadastrais.save)
 }
--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,23 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerDocument = require('./swagger.json');
 const LdapStrategy = require('passport-ldapauth');
-const passport     = require('passport');
+const passport = require('passport');
 
 app.db = db
 
 consign()
-    .include('./config/passport.js')
-    .then('./config/middlewares.js')
-    .then('./api/validation.js')
-    .then('./api')
-    .then('./config/routes.js')
-    .into(app)
+  .include('./config/passport.js')
+  .then('./config/middlewares.js')
+  .then('./api/validation.js')
+  .then('./api')
+  .then('./config/routes.js')
+  .into(app)
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(passport.initialize());
+app.post('/login', passport.login)
 
-  app.use(passport.initialize());
-  app.post('/login', passport.login) 
 app.listen(3000, () => {
-   console.log('Backend executando...')
+  console.log('Backend executando...')
 })
