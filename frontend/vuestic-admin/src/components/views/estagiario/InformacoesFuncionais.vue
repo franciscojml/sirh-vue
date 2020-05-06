79d30e0f72e0c965e463aa8d873baa208ebfc6a2
Francisco Lima <franciscojml@gmail.com>
2020-05-06 12:12:25 -0300

    Tela IFE 2

--- frontend/vuestic-admin/src/components/views/estagiario/InformacoesFuncionais.vue
+++ frontend/vuestic-admin/src/components/views/estagiario/InformacoesFuncionais.vue
@@ -34,22 +34,9 @@
                   @page-selected="readItems"
                   api-mode
                   hoverable
-                >
-                  <template slot="actions" slot-scope="props">
-                    <va-button
-                      flat
-                      small
-                      color="gray"
-                      icon="fa fa-info-circle"
-                      @click="info(props.rowData)"
-                    />
-                  </template>
-                </va-data-table>
+                ></va-data-table>
                 <div class="flex xs10 md6">
-                  <va-card
-                    :class="style12 ? 'flex xs3 mb-6' : 'flex xs7 mb-6'"
-                    color="#34495e"
-                  >
+                  <va-card :class="style12 ? 'flex xs3 mb-6' : 'flex xs7 mb-6'" color="#34495e">
                     <p class="display-6 xs10 mb-0" style="color: white;">{{ totalRecords }}</p>
                     <p>{{$t('charts.totalRecords')}}</p>
                   </va-card>
@@ -95,7 +82,8 @@ export default {
       totalRecords: 0,
       pessoa: null,
       style12: true,
-      group: []
+      group: [],
+      group2: []
     };
   },
   computed: {
@@ -104,17 +92,7 @@ export default {
         {
           name: "NU_CPF_ESTAGIARIO",
           title: this.$t("forms.comum.cpf"),
-          width: "30%"
-        },
-        {
-          name: "AFASCODI",
-          title: this.$t("forms.comum.nome"),
-          width: "70%"
-        },
-        ,
-        {
-          name: "__slot:actions",
-          dataClass: "text-right"
+          width: "15%"
         }
       ];
     },
@@ -138,9 +116,23 @@ export default {
 
       axios.get(url, { params }).then(response => {
         this.items = response.data.data;
-
-        this.group = this.$_.groupBy(this.items, 'NU_CPF_ESTAGIARIO');
-        console.log('dado' + this.group[0])
+        //this.items = this.$_.groupBy(this.items, "NU_CPF_ESTAGIARIO");
+        this.group = this.$_.groupBy(
+          this.items,
+          "NU_CPF_ESTAGIARIO"
+        );
+        
+        this.group2 = this.$_.groupBy(
+          this.items,
+          this.$_.mapObject(this.items, function(val, key) {
+            return {
+              key : val
+            };
+          })
+        );
+        //console.log("dado" + JSON.stringify(this.group2["00001288210"]));
+        console.log("group: " + JSON.stringify(this.group2));
+        
 
         this.totalPages = response.data.total_pages;
         this.totalRecords = response.data.totalRecords;
@@ -157,6 +149,13 @@ export default {
     fecharDetalhe() {
       this.style12 = true;
       this.pessoa = null;
+    },
+    getStatusColor(status) {
+      if (status != null) {
+        return "danger";
+      }
+
+      return "success";
     }
   },
   created() {
@@ -173,6 +172,16 @@ export default {
 
 .va-data-table__pagination .va-button--normal {
   padding: 0.3rem;
+}
+
+.markup-tables {
+  .va-table {
+    width: 100%;
+  }
+
+  .va-card {
+    max-width: 1200px;
+  }
 }
 </style>
 
