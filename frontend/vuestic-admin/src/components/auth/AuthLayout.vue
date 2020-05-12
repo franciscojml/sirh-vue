f74a52939dc47bf47998526d6f54c41f331cfc3f
Francisco Lima <franciscojml@gmail.com>
2020-05-12 17:16:38 -0300

    Layout tela login

--- frontend/vuestic-admin/src/components/auth/AuthLayout.vue
+++ frontend/vuestic-admin/src/components/auth/AuthLayout.vue
@@ -1,76 +1,156 @@
 <template>
-  <div class="auth-layout row align-content--center">
-    <div class="flex xs12 pa-3 flex-center">
-      <router-link class="py-5 flex-center" to="/">
-        <!--va-icon-vuestic /-->
-        <img src="/img/images/logo-sirh-erp.svg"/>
-      </router-link>
-    </div>
-
-    <div class="flex xs12 pa-3">
-      <div class="d-flex justify--center">
-        <va-card class="auth-layout__card">
-          <va-tabs
-            v-model="tabIndex"
-            center
-          >
-            <va-tab>{{ $t('auth.login') }}</va-tab>
-          </va-tabs>
-
-          <va-separator/>
-
-          <div class="pa-3">
-            <router-view/>
+  <div class="container">
+    <div class="d-flex justify-content-center h-100">
+      <div class="card">
+        <div class="card-header">
+          <h3>Sign In</h3>
+          <div class="d-flex justify-content-end social_icon">
+            <span>
+              <i class="fab fa-facebook-square"></i>
+            </span>
+            <span>
+              <i class="fab fa-google-plus-square"></i>
+            </span>
+            <span>
+              <i class="fab fa-twitter-square"></i>
+            </span>
           </div>
-        </va-card>
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="input-group form-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-user"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="username" />
+            </div>
+            <div class="input-group form-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-key"></i>
+                </span>
+              </div>
+              <input type="password" class="form-control" placeholder="password" />
+            </div>
+            <div class="row align-items-center remember">
+              <input type="checkbox" />Remember Me
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Login" class="btn float-right login_btn" />
+            </div>
+          </form>
+        </div>
+        <div class="card-footer">
+          <div class="d-flex justify-content-center links">
+            Don't have an account?
+            <a href="#">Sign Up</a>
+          </div>
+          <div class="d-flex justify-content-center">
+            <a href="#">Forgot your password?</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import VaIconVuestic from '../../iconset/VaIconVuestic'
-
-const tabs = [
-  'login'
-]
-
 export default {
-  name: 'AuthLayout',
-  components: { VaIconVuestic },
-  data () {
+  name: "AuthLayout",
+  data() {
     return {
       selectedTabIndex: 0,
-      tabTitles: ['login'],
-    }
-  },
-  computed: {
-    tabIndex: {
-      set (tabIndex) {
-        this.$router.push({ name: tabs[tabIndex] })
-      },
-      get () {
-        return tabs.indexOf(this.$route.name)
-      },
-    },
-  },
-}
+      tabTitles: ["login"]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
-.auth-layout {
-  min-height: 100vh;
-  background-image: linear-gradient(to right, #0e4ac4, #002c85);
+html,
+body {
+  background-image: url("/img/images/fundo_login.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
+  font-family: "Numans", sans-serif;
+}
 
-  &__card {
-    width: 100%;
-    max-width: 600px;
-  }
+.container {
+  height: 100%;
+  align-content: center;
+}
 
-  &__options {
-    @include media-breakpoint-down(xs) {
-      flex-direction: column;
-    }
-  }
+.card {
+  height: 370px;
+  margin-top: auto;
+  margin-bottom: auto;
+  width: 400px;
+  background-color: rgba(0, 0, 0, 0.5) !important;
+}
+
+.social_icon span {
+  font-size: 60px;
+  margin-left: 10px;
+  color: #ffc312;
+}
+
+.social_icon span:hover {
+  color: white;
+  cursor: pointer;
+}
+
+.card-header h3 {
+  color: white;
+}
+
+.social_icon {
+  position: absolute;
+  right: 20px;
+  top: -45px;
+}
+
+.input-group-prepend span {
+  width: 50px;
+  background-color: #ffc312;
+  color: black;
+  border: 0 !important;
+}
+
+input:focus {
+  outline: 0 0 0 0 !important;
+  box-shadow: 0 0 0 0 !important;
+}
+
+.remember {
+  color: white;
+}
+
+.remember input {
+  width: 20px;
+  height: 20px;
+  margin-left: 15px;
+  margin-right: 5px;
+}
+
+.login_btn {
+  color: black;
+  background-color: #ffc312;
+  width: 100px;
+}
+
+.login_btn:hover {
+  color: black;
+  background-color: white;
+}
+
+.links {
+  color: white;
+}
+
+.links a {
+  margin-left: 4px;
 }
 </style>
